567e3452570960cbadce8dce86f29f1f46fd9b25
sarat <sarat.vallabhaneni9@gmail.com>
2026-02-02 16:55:58 +0530

    changes

--- app.py
+++ app.py
@@ -16,6 +16,3 @@ def predict():
         return jsonify({'prediction': int(prediction[0])})
     except Exception as e:
         return jsonify({'error': str(e)}), 400
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
